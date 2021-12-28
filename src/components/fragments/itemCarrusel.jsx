a8f99735adbd4ddbd83a5934c3ac5d9bc0275c47
Pedro Sotelo <petter_17_18@hotmail.com>
2021-12-28 03:10:21 +0100

    recibida la información de los carruseles mediante una API

--- src/components/fragments/itemCarrusel.jsx
+++ src/components/fragments/itemCarrusel.jsx
@@ -1,20 +1,21 @@
-import Img from '../../images/1.jpg'
-
+//import Img from '../../images/1.jpg'
+const URL_IMG = "https://image.tmdb.org/t/p/w500/";
 export default function ItemCarrusel({movieInfo, index}){
+    const {original_title,backdrop_path,release_date,vote_average,overview}=movieInfo;
     return(
 
             <div className={"movie-card carrusel"+index}>
                 <div className="delante">
-                    <div className="movie-image"><img src={Img} /></div>
-                    <h4 className="movie-title">{movieInfo.title}</h4>
-                    <h6>{movieInfo.dateRealase}</h6>
-                    <div className="movie-rating">{movieInfo.rating}</div>
+                    <div className="movie-image"><img style={{width:"100%"}} src={URL_IMG+backdrop_path}/></div>
+                    <h4 className="movie-title">{original_title}</h4>
+                    <h6>{release_date}</h6>
+                    <div className="movie-rating">{vote_average}</div>
                     <button className="boton-card flip">Sinopsis</button>
                 </div>
 
                 <div className="detras">
                     <strong>
-                        {movieInfo.sinopsis}
+                        {overview}
                     </strong>
                     <br />
                     <button className="boton-card flp">Pelicula</button>
