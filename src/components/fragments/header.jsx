af37247702548bd6ad60f9e951cf332e2382dbb0
vicmacarras <65358849+vicmacarras@users.noreply.github.com>
2022-01-12 19:21:39 +0100

    En proceso header dinamico

--- src/components/fragments/header.jsx
+++ src/components/fragments/header.jsx
@@ -1,7 +1,51 @@
 import logo from '../../images/1.jpg'
 import {useState,useEffect} from 'react';
+import { API_URL_POP, API_URL_RATED, API_URL_NEW } from "../../diccionario/url";
+
 
 export default function Header(){
+    const [headerMovies,setHeaderMovies]=useState([]);
+    const [isLoading,setIsloading]=useState(true);
+
+
+/*  
+   function getMoviesFromApi(URL){
+       return(
+        fetch(URL)
+        .then(response => response.json())
+        .then(responseConverted=>responseConverted.results)
+    )}
+
+const getMoviesFromApi=(URL)=>{
+    console.log("prueba const getMoviesFromApi")
+    return(
+    fetch(URL).then(response => response.json()).then(responseConverted=>responseConverted.results)
+    )
+}
+
+    function getMovies(){
+        setIsloading(true);
+        getMoviesFromApi(API_URL_POP).then(movies=>{
+            setHeaderMovies(movies);
+            setIsloading(false);
+        })
+    }
+*/
+ 
+        
+    
+
+useEffect(()=>{
+    setIsloading(true);
+    fetch(API_URL_POP)
+    .then(response => response.json())
+    .then(responseConverted=>responseConverted.results)
+    .then(movieResults=>{
+        setHeaderMovies(movieResults);
+        setIsloading(false)});
+
+},[])
+
     return (
   <>
 
@@ -14,15 +58,31 @@ export default function Header(){
     </div>
 
     <div id="contenedor">
-      <div id="pelis_header">
-      <div className="mySlides fade">
-        <img id="foto" src={logo}/>
-        <div className="text_title">Batman</div>
-        <div className="text">1</div>
-      </div>
+        {isLoading ?
+            <p>CARGANDO....</p>
+            
+                :
+            
+         headerMovies.map(movie =>{
+            return (
+            <div id="pelis_header" key={movie.id}>
+            <div className="mySlides fade">
+            <img id="foto" src={logo}/>
+            <div className="text_title">{movie.title}</div>
+            <div className="text">{movie.vote_average}</div>
+         </div>
     </div>
+           ) 
+
+        })
+                  
+                
+
+             
+            
+}
     </div>
 
 </>
-);
+)
 }
