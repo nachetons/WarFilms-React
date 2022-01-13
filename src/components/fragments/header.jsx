524fb99a2373777cc69ff9f033abbd0c2cab7767
Pedro Sotelo <petter_17_18@hotmail.com>
2022-01-13 19:41:55 +0100

    Se pueden ver las 5 primeras películas, la funcion se puede modifar para que salgan las n que queramos. Además posteriormente borraré el comentario explicando como funciona

--- src/components/fragments/header.jsx
+++ src/components/fragments/header.jsx
@@ -9,8 +9,7 @@ const URL_IMG = "https://image.tmdb.org/t/p/w500/";
 export default function Header(){
     const [headerMovies,setHeaderMovies]=useState([]);
     const [isLoading,setIsloading]=useState(true);
-
-    const handleDragStart = (e) => e.preventDefault();
+    const [index,setIndex]=useState(0);
 
 
       
@@ -37,20 +36,44 @@ const getMoviesFromApi=(URL)=>{
         })
     }
 */
- 
-        
-    
-
+function setMovies(movieIndex){
+    let toSubstract=0;
+    if(movieIndex>5){
+        toSubstract=-7;
+        //Esto lo pongo a -7 ya que va a llegar al if en la 6ºIteracion y
+        //abajo le sumo 1 para que siga iterando en el array
+    }
+    //Utilizo la variable toSubstract para que el set del estado se ejecute en
+    //el mismo orden, ya que si se condiciona o no se reenderiza un componente
+    //en el mismo orden va a dar error sí o sí
+    setIndex(lastIndex=>lastIndex+toSubstract+1);
+}
 useEffect(()=>{
     setIsloading(true);
     fetch(API_URL_POP)
     .then(response => response.json())
     .then(responseConverted=>responseConverted.results)
     .then(movieResults=>{
-        setHeaderMovies(movieResults);
-        setIsloading(false)});
+            setHeaderMovies(movieResults);
+            setIsloading(false)});
 
-},[])
+},[]);
+
+
+useEffect(() => {
+    const interval = setInterval(() => {
+        setMovies(index);
+    }, 8000);
+    return () => clearInterval(interval);
+}, [index]);
+    
+    
+    
+
+
+
+
+    
 
     return (
   <>
@@ -61,74 +84,23 @@ useEffect(()=>{
       <div className="background">
 
       </div>
-    </div>
+</div>
 
     <div id="contenedor">
         {isLoading ?
             <p>CARGANDO....</p>
             
                 :
-                /*<AliceCarousel
-        autoPlay
-        autoPlayStrategy="none"
-        autoPlayInterval={1000}
-        animationDuration={1000}
-        animationType="fadeout"
-        infinite
-        touchTracking={false}
-        disableDotsControls
-        disableButtonsControls
-        items={items}
-    />*/
-
-        <AliceCarousel 
-        autoPlay 
-        autoPlayInterval={2700} 
-        animationType="fadeout"
-        disableDotsControls
-        disableButtonsControls
-        infinite
-         className="slider_content">
-            {
-headerMovies.map(movie =>{
-
-    return (
-    <div id="pelis_header" key={movie.id}>
-    <div className="mySlides fade">
-    <img id="foto" src={URL_IMG+movie.poster_path}/>
-    <div className="text_title">{movie.title}</div>
-    <div className="text">{movie.vote_average}</div>
- </div>
-</div>
-   ) 
-
-})
-
-
-
-
-            }
-        </AliceCarousel>
-       /*  headerMovies.map(movie =>{
-
-            return (
-            <div id="pelis_header" key={movie.id}>
-            <div className="mySlides fade">
-            <img id="foto" src={logo}/>
-            <div className="text_title">{movie.title}</div>
-            <div className="text">{movie.vote_average}</div>
-         </div>
-    </div>
-    
-           ) 
-
-        })*/
-      
-                
-
-             
-            
-}
+            <div className='slider_content'>
+                <div id="pelis_header" key={headerMovies[index].id}>
+                    <div className="mySlides fade">
+                        <img id="foto" src={URL_IMG+headerMovies[index].poster_path}/>
+                        <div className="text_title">{headerMovies[index].title}</div>
+                    <div className="text">{headerMovies[index].vote_average}</div>
+                    </div>
+                </div>
+            </div> 
+        }
     </div>
 
 </>
