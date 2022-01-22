f48f3eb1be2a8d611dce5ee26c09ab8add4549c7
nachetons <tripleignacio@gmail.com>
2022-01-22 19:14:00 +0100

    integrada cache

--- src/components/fragments/fetchApi.jsx
+++ src/components/fragments/fetchApi.jsx
@@ -1,6 +1,7 @@
 import { URL_IMG } from '@/diccionario/url';
 import { useEffect, useState } from 'react';
 
+const cache = {};
 
 export default function fetchApi(URL) {
 
@@ -8,14 +9,22 @@ export default function fetchApi(URL) {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-      if (!URL) return;
-      const fetchData = async () => {
-          setStatus('fetching');
-          const response = await fetch(URL);
-          const data = await response.json();
-          setData(data.results);
-          setStatus('fetched');
-      };
+    if (!URL) return;
+
+        const fetchData = async () => {
+            setStatus('fetching');
+            if (cache[URL]) {
+                const data = cache[URL];
+                setData(data.results);
+                setStatus('fetched');
+            } else {
+                const response = await fetch(URL);
+                const data = await response.json();
+                cache[URL] = data; // set response in cache;
+                setData(data.results);
+                setStatus('fetched');
+            }
+        };
 
       fetchData();
   }, [URL]);
