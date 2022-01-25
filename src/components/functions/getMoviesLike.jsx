976655e3715a713b538254b21a3764bf9eb84543
nachetons <tripleignacio@gmail.com>
2022-01-25 03:08:01 +0100

    marcadores getPeliculas de firebase funcionando

---
+++ src/components/functions/getMoviesLike.jsx
@@ -0,0 +1,36 @@
+// import { getFirestore, collection, doc, setDoc } from 'firebase/firestore'
+import { auth } from '../../../config'
+import { getDatabase, ref, child, get } from 'firebase/database'
+import Marcadores from '@/components/pages/marcadores'
+import React, { useState, useEffect } from 'react'
+
+export default function getMoviesLike (setIsAuth, isAuth) {
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    fetchMovies()
+  }, [])
+  const Uid = auth.currentUser.uid
+  const dbRef = ref(getDatabase())
+  const fetchMovies = async () => {
+    get(child(dbRef, 'likes/' + Uid)).then((snapshot) => {
+      if (snapshot.exists()) {
+        snapshot.forEach((childSnapshot) => {
+          setMovies(childSnapshot.val())
+        })
+
+        return (
+
+          <Marcadores setIsAuth={setIsAuth} isAuth={isAuth} movies={movies} />
+        )
+      } else {
+        console.log('No data available')
+      }
+    }).catch((error) => {
+      console.error(error)
+    })
+  }
+
+  /* const likesRef = collection(dbs, 'likes')
+  const docRef = doc(likesRef, Uid.toString())
+  setDoc(docRef, infoLike) */
+}
--- src/components/pages/marcadores.jsx
+++ src/components/pages/marcadores.jsx
@@ -5,50 +5,89 @@ import '@/styles/login.css'
 import '@/styles/main.css'
 import '@/styles/navs.css'
 import '@/styles/opciones.css'
+import { auth } from '../../../config'
 import Footer from '@/components/fragments/footer'
 import Navs from '@/components/fragments/navs/navs.jsx'
+import { useState, useEffect } from 'react'
+import { getDatabase, ref, child, get } from 'firebase/database'
 
 const marcadores = ({ setIsAuth, isAuth }) => {
   const redirectPage = useHistory()
+  const [isLoading, setIsloading] = useState(true)
+  const [movies, setMovies] = useState([])
+  const dataMovies = []
+
   if (isAuth === false) {
     redirectPage.push('/')
+  } else {
+    const Uid = auth.currentUser.uid
+    const dbRef = ref(getDatabase())
+
+    function fetchMovies () {
+      get(child(dbRef, 'likes/' + Uid)).then((snapshot) => {
+        if (snapshot.exists()) {
+          snapshot.forEach((childSnapshot) => {
+            dataMovies.push(childSnapshot.val())
+          })
+          setMovies(dataMovies)
+          setIsloading(false)
+        } else {
+          console.log('No data available')
+        }
+      }).catch((error) => {
+        console.error(error)
+      })
+    }
+
+    useEffect(() => {
+      fetchMovies()
+    }, [])
   }
 
   return (
     <>
+      <div className='row' id='contenedor_main'>
 
-      <Navs setIsAuth={setIsAuth} isAuth={isAuth} />
+        <h3 id='titulos' className='titulo'>Peliculas</h3>
+        <Navs setIsAuth={setIsAuth} isAuth={isAuth} />
 
-      <div className='container'>
+        {
 
-        <form className='modal-content' id='form_content' action='#'>
-          <div className='imgcontainer'>
-            <i className='fas fa-user fa-2x foto' title='Portafolio' />
-            <br />
-          </div>
+        !isLoading
+          ? movies && movies.map((movie, index) => {
+              console.log(movie.title)
 
-          <div className='container'>
-            <label htmlFor='uname'><b>Peliculas favoritas: </b></label>
-            <ul>
-              <li>Pelicula1</li>
-              <li>Pelicula2</li>
-              <li>Pelicula3</li>
-              <li>Pelicula4</li>
-              <li>Pelicula5</li>
-            </ul>
-            <br />
+              return (
 
-            <button className='boton-form' type='submit'>
-              Guardar cambios
-            </button>
-            <input type='checkbox' /> Remember me
-          </div>
+                <div key={index} className='peliculas' id='list_pelis'>
+                  <div className='container'>
+                    <div className='card'>
+                      <div className='card-body'>
+                        <h5 className='card-title'>{movie.title}</h5>
+                        <p className='card-text'>{movie.overview}</p>
+                      </div>
+                    </div>
+                  </div>
 
-          <div className='container' />
-        </form>
+                </div>
+              )
+            })
 
+          : (
+            <div className='container'>
+              <div className='card'>
+                <div className='card-body'>
+                  <br />
+                  <br />
+                  <h5 className='card-title'>No hay peliculas en tu lista</h5>
+                  <p className='card-text'>Agrega peliculas a tu lista de favoritos</p>
+                </div>
+              </div>
+            </div>
+            )
+
+     }
       </div>
-
       <Footer />
     </>
 
